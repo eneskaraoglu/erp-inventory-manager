6ed19657edacaa98c041f4b84b4e81aae9604a1f
enes <enes@bilisim.com.tr>
2025-12-26 14:58:17 +0300

    Step 1 one
    Step 1 one

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import ProductCard from './components/ProductCard'
 
 function App() {
-  const [count, setCount] = useState(10)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-          <h2>Count: {count}</h2>
-          <button onClick={() => setCount(count + 1)}>+ Increase</button>
-          <button onClick={() => setCount(count - 1)}>- Decrease</button>
-          <button onClick={() => setCount(0)}>Reset</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  return(
+  <div>
+    <h1>ERP Inventory Manager</h1>
+    <ProductCard/>
+  </div>
   )
 }
 
---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,10 @@
+function ProductCard(){
+    return(
+        <div>
+            <h3>Product name</h3>
+            <p>Price: £9.99</p>
+            <p>Quantity: 50</p>
+        </div>
+    )
+}
+export default ProductCard
